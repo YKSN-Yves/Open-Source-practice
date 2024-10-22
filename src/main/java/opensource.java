a5f6da3db1ddace6499cd7a79d94d9a26c909d54
ZYiii <2771467304@qq.com>
2024-10-22 21:25:42 +0800

    主分支

--- src/main/java/opensource.java
+++ src/main/java/opensource.java
@@ -1,8 +1,7 @@
 
 public class opensource {
     public static void main(String[] args) {
-        System.out.println("branch2新增了这一句代码");
-        System.out.println("branch2删掉了之前那句，重新说了Hello OPEN SOURCE!");
+        System.out.println("Hello OPEN SOURCE!");
     }
 
 }
