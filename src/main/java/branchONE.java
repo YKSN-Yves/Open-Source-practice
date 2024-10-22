a260f88fdbdad6d9f133904489d8f65eb41ad701
ZYiii <2771467304@qq.com>
2024-10-22 21:14:15 +0800

    branch3

--- src/main/java/branchONE.java
+++
@@ -1,6 +0,0 @@
-public class branchONE {
-    public static void main(String[] args) {
-        System.out.println("这是branch1创建的文件");
-        System.out.println("这是branch1编写的代码，branch2修改了这一行代码，新增一句话");
-    }
-}
---
+++ src/main/java/branchTHREE.java
@@ -0,0 +1,6 @@
+public class branchTHREE {
+    public static void main(String[] args) {
+        System.out.println("这是branch3创建的文件");
+        System.out.println("branch3删除了之前branch1创建的文件");
+    }
+}
