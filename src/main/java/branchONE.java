ec060806fef5f93147d48b2e8b4a03e24e542ee4
ZYiii <2771467304@qq.com>
2024-10-22 21:12:27 +0800

    修改代码

--- src/main/java/branchONE.java
+++ src/main/java/branchONE.java
@@ -1,6 +1,6 @@
 public class branchONE {
     public static void main(String[] args) {
         System.out.println("这是branch1创建的文件");
-        System.out.println("这是branch1编写的代码");
+        System.out.println("这是branch1编写的代码，branch2修改了这一行代码，新增一句话");
     }
 }
--- src/main/java/opensource.java
+++ src/main/java/opensource.java
@@ -1,7 +1,8 @@
 
 public class opensource {
     public static void main(String[] args) {
-        System.out.println("Hello OPEN SOURCE!");
+        System.out.println("branch2新增了这一句代码");
+        System.out.println("branch2删掉了之前那句，重新说了Hello OPEN SOURCE!");
     }
 
 }
