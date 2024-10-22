2edf872397aeddd812d6e210b78c2cda4fca4f22
ZYiii <2771467304@qq.com>
2024-10-22 21:30:23 +0800

    Revert "Update branchONE.java"
    This reverts commit e0b7a4fa276c2c108cea2e680d6125604f99180e.

--- src/main/java/branchONE.java
+++ src/main/java/branchONE.java
@@ -1,9 +1,8 @@
 public class branchONE {
     public static void main(String[] args) {
-
+        System.out.println("这是branch1创建的文件");
+        System.out.println("这是branch1编写的代码");
         System.out.println("这是branch1编写的代码，branch2修改了这一行代码，新增一句话");
-
-        System.out.println("现在branch3在尝试修改代码，删除了很多但是提交后发现不小心删错了，要进行回退");
 
     }
 }
